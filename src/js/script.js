0ad543aff4cc9db27a2661c31fe5fd5c4c4d253c
Guilherme <anglim.guilerme@gmail.com>
2025-09-10 10:48:48 -0300

    8-Realizando JS (parte2)

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,52 @@
-//declarando as variaveis com dom (document model object)//
-const carrossellista = document.querySelector(".carrossel-lista");
-const carrosselitem = document.querySelectorALL(".carrossel-item");
-const botaoanteruir = document.getElementsByClassName("btn-anterior");
-const botaoproximo = document.getElementsByClassName("btn-proximo");
+// array de objetos com as imagens e informações do banner [array] {objeto} - possui atributos
+const bannerItems = [
+{
+    imagem:"../assets/Game1.jpg",
+    titulo:"Titulo do filme 1",
+    descricao:"Filme 1"
+},
+{
+    imagem:"../assets/Game2.jpg",
+    titulo:"Titulo do filme 2",
+    descricao:"Filme 2"
+},
+{
+    imagem:"../assets/Game3.jpg",
+    titulo:"Titulo do filme 3",
+    descricao:"Filme 3"
+}
 
-let contador=0;
-const larguraItem=carrosselitem[0] .offsetWidth+10; //largura do item + margem, definição
+];
 
-botaoproximo.addEventListener("click",()=>{
-    if (contador < carrosselitem.length -1){
-        contador++;
-        carrossellista.computedStyleMap.transform='translateX (-${contador * larguraitem}px)';
+//declarando as variáveis e elementos com DOM -- "get element by id" (Document object mod)
+const tempo = 5000; //tempo em milissegundos
+const elementoBanner = document.querySelector(".banner"); // pega a primeira classe/id chamada .banner
+const elementoTitulo = document.querySelector(".banner-titulo");
+const elementoDescricao = document.querySelector (".banner-descricao");
+let i=0; // inicio da contagem das imagens!!
 
+// função para o slideshow
+
+function slideShow(){
+    //altera a imagem de fundo do banner
+    // `` - templete strings ou strings literais!
+    // `url($ {bannerItems}/)` -- concatena, juntando banner items tbm, permitindo o acesso as imagens (url)!!
+    elementoBanner.computedStyleMap.backgroundImage = `url($ {bannerItems[i].imagem}/)`
+    //altera o titulo do banner, acessa cada parte (titulo, descricao...)
+    elementoTitulo.textContent = bannerItems [i].titulo;
+    elementoDescricao.textContent = bannerItems[i].descricao;
+
+    //incrementa o indice (i)- zerado, e reinicia quando chegar no final do array. ++ = incremento
+    // irá começar no zero, ir para proxima (1) lendo a primeira img, pra proxima (2), pra proxima (3) até chegar ao fim, dps repete para a primeira
+    // vai continuar incrementando valores até atingir 1, 2 ou 3!
+    // se o i for maior que a quantidade de imagens, volta ao início!!!
+    i++;
+    if (i >= bannerItems.length){
+        i = 0;
     }
-})
+    //chama a função novamente após o tempo definido
+    setTimeout(slideShow,tempo);
 
-botaoproximo.addEventListener("click",()=>{
-    if (contador > 0){
-        contador--;
-        carrossellista.computedStyleMap.transform='translateX (-${contador * larguraitem}px)';
-
-    }
-})
-
-
+}
+//inicia o slide show
+slideShow()
